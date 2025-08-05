62a1b8d5cee1b2423bf81155d7174563843533c2
Arpit Paliwal <133906733+Arpit0408@users.noreply.github.com>
2025-08-06 03:35:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 const port = process.env.PORT || 5000;
 
 const corsOptions = {
-origin: ['http://localhost:5173', 'http://localhost:5000'],
+origin: ['https://chatkro-ochre.vercel.app/', 'http://localhost:5000'],
   methods: ['GET','POST','PUT','DELETE','OPTIONS'],
   allowedHeaders: ['Content-Type','Authorization'],
   credentials: true, // if you're using cookies/tokens
@@ -45,7 +45,7 @@ const server = app.listen(port, () => {
 const io =require('socket.io')(server,{
   pingTimeout: 60000,
   cors: {
-    origin:"http://localhost:5173"
+    origin:"https://chatkro-ochre.vercel.app/"
   }
 });
 
