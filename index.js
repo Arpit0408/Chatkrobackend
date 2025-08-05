d4bc08c0fd27031a7d4a7d01533d15d4ddd878c6
Arpit Paliwal <133906733+Arpit0408@users.noreply.github.com>
2025-08-06 03:43:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 const port = process.env.PORT || 5000;
 
 const corsOptions = {
-origin: ['https://chatkro-ochre.vercel.app/', 'http://localhost:5000'],
+origin: ['https://chatkro-ochre.vercel.app/', 'https://chatkrobackend.onrender.com/'],
   methods: ['GET','POST','PUT','DELETE','OPTIONS'],
   allowedHeaders: ['Content-Type','Authorization'],
   credentials: true, // if you're using cookies/tokens
